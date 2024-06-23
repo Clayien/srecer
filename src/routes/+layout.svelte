a8581925e341940c611fd1fc4501111a0a423c38
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-06-23 08:46:39 +0530

    feat: add poppins and new colors

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,15 @@
 	import '../app.css';
 </script>
 
-<slot></slot>
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
 
-<style></style>
+<main class="bg-black-900 text-grey-600 h-screen w-screen overflow-hidden font-sans leading-loose">
+	<slot />
+</main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,8 +4,22 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		colors: {
+			black: {
+				700: '#222222',
+				800: '#181818',
+				900: '#111111'
+			},
+			grey: {
+				600: '#BCBCBC',
+				900: '#777777'
+			},
+			red: '#BF3737',
+			green: '#22D234'
+		},
+		fontFamily: {
+			sans: ['Poppins', 'sans-serif']
+		}
 	},
-
 	plugins: []
 } as Config;
